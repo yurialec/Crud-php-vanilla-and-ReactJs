f225decf96b19408c08fda770fcc2809c791a148
Yuri Alec <yurialec@hotmail.com>
2022-10-08 16:33:20 -0300

    adicionado react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'; 
 import {Home} from './pages/Home'
 
 function App() {
 
   return (
     <div>
-      
+      <Router>
+        <Switch>
+          <Route path="/" component={Home}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
